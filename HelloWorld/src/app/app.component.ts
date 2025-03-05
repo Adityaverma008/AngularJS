65327ab33e6469da076e6d05860959fde31460a2
Adityaverma008 <Adityaverma008@users.noreply.github.com>
2025-03-05 16:44:53 +0530

    [Aditya verma]adding uc2

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'HelloWorld';
+  // uc-2 
+  // message: string = 'Hello from BridgeLabz';
+  ngOnInit(): void{
+    this.title = 'Hello From BridgeLab(data binding)';
+  }
 }
